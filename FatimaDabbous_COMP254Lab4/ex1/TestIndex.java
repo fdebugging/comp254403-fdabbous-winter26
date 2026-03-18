9feb0f494dcd256ea6d22209c7ab3b2d317a8656
fdebugging <fatima_dabbous@hotmail.com>
2026-03-18 19:40:52 -0400

    Complete Exercise1

---
+++ FatimaDabbous_COMP254Lab4/ex1/TestIndex.java
@@ -0,0 +1,14 @@
+public class TestIndex {
+    public static void main(String[] args) {
+        LinkedPositionalList<String> list = new LinkedPositionalList<>();
+
+        Position<String> p1 = list.addLast("A");
+        Position<String> p2 = list.addLast("B");
+        Position<String> p3 = list.addLast("C");
+
+        System.out.println("List: " + list);
+        System.out.println("Index of A: " + list.indexOf(p1));
+        System.out.println("Index of B: " + list.indexOf(p2));
+        System.out.println("Index of C: " + list.indexOf(p3));
+    }
+}
