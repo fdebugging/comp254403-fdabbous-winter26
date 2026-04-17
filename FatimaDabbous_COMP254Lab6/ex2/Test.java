660433d57d1ba9dbb54dcfcdecc0caebd83acdc4
fdebugging <fatima_dabbous@hotmail.com>
2026-04-16 22:37:20 -0400

    Import Classes For Ex1

--- FatimaDabbous_COMP254Lab6/ex2/Test.java
+++ FatimaDabbous_COMP254Lab6/ex2/Test.java
@@ -11,8 +11,8 @@ public class Test {
         map.put(20, null);
         map.put(30, "Banana");
 
-        System.out.println("Key 10 exists: " + map.containsKey(10)); // true
-        System.out.println("Key 20 exists: " + map.containsKey(20)); // true
-        System.out.println("Key 40 exists: " + map.containsKey(40)); // false
+        System.out.println("Key 10 exists: " + map.containsKey(10)); 
+        System.out.println("Key 20 exists: " + map.containsKey(20)); 
+        System.out.println("Key 40 exists: " + map.containsKey(40)); 
     }
 }
